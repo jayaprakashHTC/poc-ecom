b916243662e70c1f6c67f436a464665edfb0b57c
Jayaprakash Galla <jayaprakash.galla@htcindia.local>
2022-10-11 17:21:38 +0530

    new auto dropdown changes

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -3,9 +3,12 @@ import { BsSearch } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getMenuData } from "../store/action/usersActions";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import PropTypes from "prop-types";
 
 const SearchSection = () => {
     const navigate = useNavigate();
+    const products = useSelector(state => state.productsReducers.products);
     const [search, setSearch] = useState(null);
     const [category, setCategory] = useState(null);
     const menus = useSelector(state => state.menuReducers.menus);
@@ -17,30 +20,75 @@ const SearchSection = () => {
         data();
     },[data]);
   
+    const handleOnSearch = (string, results) => {
+        setSearch(string);
+    };
+    const handleOnSelect = (item) => {
+        console.log("item", item);
+    };
+   
   
+
     const handleSubmit = () =>{
         navigate(`/search/${search}`, { state: { category: category, search:search } });
     };
     return (
         <div className="search-bar">
-            <div>
-                <select onChange={e => setCategory(e.target.value)} className="formcontrol selectdrop">
-                    <option value="all">All</option>
-                    {
-                        menus.map(cat => <option key={cat}>{cat}</option>)
-                    }
-                </select>
-                <input
+            <div className="flex-container">
+                <div>
+                    <select onChange={e => setCategory(e.target.value)} className="formcontrol selectdrop">
+                        <option value="all">All</option>
+                        {
+                            menus.map(cat => <option key={cat}>{cat}</option>)
+                        }
+                    </select>
+                </div>
+               
+                {/* <input
                     type="search"
                     placeholder="search..."
                     onChange={e => setSearch(e.target.value)}
                     className="formcontrol input"
-                />
-                <button type="button" className="formcontrol search-btn" onClick={handleSubmit}><BsSearch /></button>
+                /> */}
+                <div>
+                    <ReactSearchAutocomplete
+                        items={products}
+                        onSearch={handleOnSearch}
+                        onSelect={handleOnSelect}
+                        fuseOptions={{ keys: ["title", "category"] }}
+                        resultStringKeyName="title" 
+                        showIcon={false}
+                        styling={{
+                            height: "34px",
+                            border: "1px solid darkgreen",
+                            borderRadius: "4px",
+                            backgroundColor: "white",
+                            boxShadow: "none",
+                            hoverBackgroundColor: "lightgreen",
+                            color: "darkgreen",
+                            fontSize: "12px",
+                            fontFamily: "Courier",
+                            iconColor: "green",
+                            lineColor: "lightgreen",
+                            placeholderColor: "darkgreen",
+                            clearIconMargin: "3px 8px 0 0",
+                            zIndex: 2,
+                        }}
+                    
+                    />
+                </div>
+                <div>
+                    <button type="button" className="formcontrol search-btn" onClick={handleSubmit}><BsSearch /></button>
+                </div>
+               
                 
             </div>
         </div>
     );
+};
+
+SearchSection.propTypes = {
+    autoFocus: PropTypes.any
 };
 
 export default SearchSection;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React, {lazy, Suspense} from "react";
 import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import CheckoutIndex from "../pages/checkout/index.js";
 // import SearchIndex from "../pages/search/index.js";
 // import CateoryIndex from "../pages/category/index.js";
 // import Home from "../pages/index.jsx";
@@ -22,7 +23,7 @@ const RouterPage = () => {
     const ProductIndex = lazy(() =>import("../pages/products/index.jsx"));
     const CartIndex = lazy(() =>import("../pages/cart/index.js"));
     const Login = lazy(() =>import("../components/Login.jsx"));
-    const Checkout = lazy(() =>import("../components/Checkout.jsx"));
+    const CheckoutIndex = lazy(() =>import("../pages/checkout/index.js"));
     const CateoryIndex= lazy(() =>import("../pages/category/index.js"));
     const SearchIndex= lazy(() =>import("../pages/search/index.js"));
     return (
@@ -34,7 +35,7 @@ const RouterPage = () => {
                 <Route exact path="/" element={<Suspense fallback={<h2 className="text-center">Loading..</h2>}><Home/></Suspense>} />
                 <Route exact path="/oneproduct/:id" element={<Suspense fallback={<h2 className="text-center">Loading..</h2>}><ProductIndex/></Suspense>} />
                 <Route exact path="/cart" element={<Suspense fallback={<h2 className="text-center">Loading..</h2>}><CartIndex/></Suspense>} />
-                <Route exact path="/checkout" element={<Suspense fallback={<h2 className="text-center">Loading..</h2>}><Checkout/></Suspense>} />
+                <Route exact path="/checkout" element={<Suspense fallback={<h2 className="text-center">Loading..</h2>}><CheckoutIndex /></Suspense>} />
                 <Route exact path="/category/:cat" element={<Suspense fallback={<h2 className="text-center">Loading..</h2>}><CateoryIndex /></Suspense>} />
                 <Route exact path="/search/:search" element={<Suspense fallback={<h2 className="text-center">Loading..</h2>}><SearchIndex /></Suspense>} />
             </Routes> 
